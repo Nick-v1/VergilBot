29246338afec6e381de3bba1e5eb53013f6a45e2
Nick-v1 <nikos.voulgaridis1@gmail.com>
2022-12-04 14:56:43 +0200

    Add project files.

---
+++ Modules/Commands.cs
@@ -0,0 +1,98 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+
+namespace Vergil
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+
+            await ReplyAsync("pong");
+        }
+
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo) => ReplyAsync(echo);
+
+        [Command("button")]
+        public async Task CreateNoButton(string buttonstr)
+        {
+            var builder = new ComponentBuilder().WithButton(buttonstr, "custom-button");
+            var s = new SelectMenuOptionBuilder();
+            await ReplyAsync("\t", components: builder.Build());
+        }
+    }
+
+    [Group("Math")]
+    public class SampleModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("square")]
+        [Summary("Squares a number.")]
+        public async Task SquareAsync(
+        [Summary("The number to square.")]
+        int num)
+        {
+            // We can also access the channel from the Command Context.
+            await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+        }
+        
+    }
+
+    [Group("users")]
+    public class UsersModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(SocketUser user = null) 
+        {
+            
+            var userInfo = user ?? Context.Client.CurrentUser;   // if (user != null) userInfo = user else new Context(); //if whatever is to the left of is not null, use that, otherwise use what's to the right.
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+            
+        }
+    }
+
+    [Group("admin")]
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Group("clean")]
+        public class CleanModule : ModuleBase<SocketCommandContext> 
+        {
+            [Command]
+            public async Task DefaultCleanAsync()
+            { 
+            
+            }
+
+            [Command("messages")]
+            public async Task CleanAsync(int count)
+            {
+                var messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Before, count).FlattenAsync();
+
+                foreach (var message in messages)
+                {
+                    await Context.Channel.DeleteMessageAsync(message.Id);
+                }
+
+            } 
+        }
+
+        [Command("purge")]
+        public async Task PurgeAsync()
+        {
+            var messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Around).FlattenAsync();
+            var filteredMessages = messages.Where(x => (DateTimeOffset.UtcNow - x.Timestamp).TotalDays <= 14);
+
+            if (filteredMessages.Count() == 0) await ReplyAsync("Nothing to delete!");
+
+            else
+            {
+                await (Context.Channel as ITextChannel).DeleteMessagesAsync(filteredMessages);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    private DiscordSocketClient _client;
+    private IServiceCollection _collection;
+    private CommandService _commands;
+    private DiscordSocketConfig _config;
+    private IServiceProvider _services;
+    private static IConfigurationRoot builder;
+
+    private static void Main(string[] args) 
+    {
+        //builder to access client secrets
+        builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddUserSecrets<Program>().Build();
+
+       
+        new Program().MainAsync().GetAwaiter().GetResult(); 
+    
+    }
+
+
+    public async Task MainAsync()
+    {
+        
+        _config = new DiscordSocketConfig() { GatewayIntents = GatewayIntents.All };
+
+        _collection = new ServiceCollection()
+            .AddSingleton(_config)
+            .AddSingleton<DiscordSocketClient>();
+
+        _services = _collection.BuildServiceProvider();
+
+        _commands = new CommandService();
+
+        _client = new DiscordSocketClient(_config);
+
+        _client.Log += ClientLog;
+
+
+        //use builder to get discord token;
+        string token = builder.GetSection("DISCORD_TOKEN").Value;
+        
+        await InstallCommandsAsync();
+        
+        await _client.LoginAsync(TokenType.Bot, token); //GetEnvironmentVariable("token");
+        await _client.StartAsync();
+
+
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    private Task ClientLog(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    public async Task InstallCommandsAsync()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    public async Task HandleCommandAsync(SocketMessage messageParam)
+    {
+
+        var message = messageParam as SocketUserMessage;
+
+        if (message == null) return;
+
+        int argPos = 6;
+        
+        //two ways of calling the bot
+        if (message.HasStringPrefix("vergil ", ref argPos))
+        {
+            var context = new SocketCommandContext(_client, message);
+
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine(result.ErrorReason);
+            }
+            if (result.Error.Equals(CommandError.UnmetPrecondition))
+            {
+                await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+        int argPos1 = 1;
+        if (message.HasStringPrefix("v ", ref argPos1))
+        {
+            var context = new SocketCommandContext(_client, message);
+
+            var result = await _commands.ExecuteAsync(context, argPos1, _services);
+
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine(result.ErrorReason);
+            }
+            if (result.Error.Equals(CommandError.UnmetPrecondition))
+            {
+                await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+
+        }
+    }
+}
