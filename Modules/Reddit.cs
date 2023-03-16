da9fe7133e3add6268721a21b3352fa35691e38f
Nick-v1 <nikos.voulgaridis1@gmail.com>
2023-03-17 00:06:17 +0200

    Reddit module improvement

--- Modules/Reddit.cs
+++ Modules/Reddit.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Net;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Linq;
 using VergilBot.Models.Misc;
@@ -29,55 +30,64 @@ namespace VergilBot.Modules
             {
                 httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
 
-                var response = await httpClient.GetStringAsync(url);
-
-                JObject obj = JObject.Parse(response);
-
-                JArray posts = (JArray)obj["data"]["children"];
-
-                /*Random random = new Random();
-                JToken post = posts[random.Next(posts.Count)];*/
-
-                var random = ThreadLocalRandom.Next(0, posts.Count);
-                JToken post = posts[random];
-
-                // Get the title and author of the post
-                string title = post["data"]["title"].ToString();
-                string author = post["data"]["author"].ToString();
-                string subreddit_name_prefixed = post["data"]["subreddit_name_prefixed"].ToString();
-                string permalink = post["data"]["permalink"].ToString();
-                string redditPostUrl = $"https://www.reddit.com/{permalink}";
-                string bodyText = post["data"]["selftext"].ToString();
-
-
-                // Image checking
-                string imageUrl = null;
-                if (post["data"]["url"].ToString().EndsWith(".jpg") ||
-                    post["data"]["url"].ToString().EndsWith(".jpeg") ||
-                    post["data"]["url"].ToString().EndsWith(".png"))
+                try
                 {
-                    imageUrl = post["data"]["url"].ToString();
+                    var response = await httpClient.GetStringAsync(url);
+
+                    JObject obj = JObject.Parse(response);
+
+                    JArray posts = (JArray)obj["data"]["children"];
+
+                    /*Random random = new Random();
+                    JToken post = posts[random.Next(posts.Count)];*/
+
+                    var random = ThreadLocalRandom.Next(0, posts.Count);
+                    JToken post = posts[random];
+
+                    // Get the title and author of the post
+                    string title = post["data"]["title"].ToString();
+                    string author = post["data"]["author"].ToString();
+                    string subreddit_name_prefixed = post["data"]["subreddit_name_prefixed"].ToString();
+                    string permalink = post["data"]["permalink"].ToString();
+                    string redditPostUrl = $"https://www.reddit.com/{permalink}";
+                    string bodyText = post["data"]["selftext"].ToString();
+                    string urlPost = (string)post["data"]["url"];
+
+
+                    // Image type checking
+                    string imageOrGifOrVideo = null;
+                    if (urlPost.EndsWith(".gif") || urlPost.EndsWith(".gifv") || urlPost.EndsWith(".mp4") || urlPost.EndsWith(".png") || urlPost.EndsWith(".jpg") || urlPost.EndsWith(".jpeg")
+                        || urlPost.Contains("youtube.com") || urlPost.Contains("youtu.be"))
+                    {
+                        imageOrGifOrVideo = urlPost;
+                    }
+
+
+                    embed
+                        .WithTitle(title)
+                        .WithColor(Color.Orange)
+                        .WithFooter($"from: {subreddit_name_prefixed} by {author}\n" +
+                        $"{botUser.Username}#{botUser.Discriminator}", botUser.GetAvatarUrl())
+                        .WithCurrentTimestamp()
+                        .WithUrl(redditPostUrl);
+
+
+                    if (imageOrGifOrVideo != null)
+                    {
+                        embed.WithImageUrl(imageOrGifOrVideo);
+                    }
+
+
+                    if (!string.IsNullOrEmpty(bodyText))
+                        embed.WithDescription(bodyText);
+                    else
+                        embed.WithDescription($"{post["data"]["url_overridden_by_dest"]}");
+
                 }
-
-
-                embed
-                    .WithTitle(title)
-                    .WithDescription($"{post["data"]["url_overridden_by_dest"]}")
-                    .WithColor(Color.Orange)
-                    .WithFooter($"from: {subreddit_name_prefixed} by {author}\n" +
-                    $"{botUser.Username}#{botUser.Discriminator}", botUser.GetAvatarUrl())
-                    .WithCurrentTimestamp()
-                    .WithUrl(redditPostUrl);
-
-                if (imageUrl != null)
+                catch (Exception ex)
                 {
-                    embed.WithImageUrl(imageUrl);
+                    return embed.WithTitle(ex.Message).WithColor(Color.Red);
                 }
-
-                if (!string.IsNullOrEmpty(bodyText))
-                    embed.WithDescription(bodyText);
-
-
             }
             return embed;
         }
