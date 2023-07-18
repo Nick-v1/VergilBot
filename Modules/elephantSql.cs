d245da30db8deaa29a3059206c7edd1d698048a4
nikosv <nikosv@arx.net>
2023-07-18 23:28:52 +0300

    Hid connection strings for discord bot and database db

--- Modules/elephantSql.cs
+++ Modules/elephantSql.cs
@@ -156,8 +156,7 @@ namespace VergilBot.Modules
         private static string Get() 
         {
             IConfigurationRoot config = Program.GetConfiguration();
-            //var uriString = config.GetSection("ELEPHANTSQL_CON").Value; //moved connection string to user secrets
-            var uriString = "postgres://pcbiyqtd:kqBXoSPMk3KgaFBUt3nv8mCKT-NI_YhG@dumbo.db.elephantsql.com/pcbiyqtd";
+            var uriString = config.GetSection("ELEPHANTSQL_CON").Value; //moved connection string to user secrets
             var uri = new Uri(uriString);
             var db = uri.AbsolutePath.Trim('/');
             var user = uri.UserInfo.Split(':')[0];
